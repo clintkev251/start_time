a319495ed72f63152ecc339e945f55647a115d55
Unknown <kjc008@aquinas.edu>
2019-01-24 14:23:23 -0500

    Image changed again, admin changes non functional

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ body, html {
 
 .bg-image {
   /* The image used */
-  background-image: url("https://farm5.staticflickr.com/4891/39745614053_7f1e3b0d14_z_d.jpg");
+  background-image: url("https://farm5.staticflickr.com/4888/32040173048_b4a45010a0_z_d.jpg");
   
   /* Add the blur effect */
   filter: blur(8px);
