e4a6a2d11d363bc0b929b27aed12a5d4784486fc
Unknown <kjc008@aquinas.edu>
2019-02-11 17:11:04 -0500

    Removed unnecessary code style and functionality changes on buttons
    Accessibility best practices implemented

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Start Time</title>
         <!-- TODO: Selectable day?? -->
         <?php ; 
-        $datetime = new DateTime('tomorrow');
-        //$tomorrow = $datetime->format('m-d-Y');
         $myFile = "admin/start.txt";
-        $startOB = "admin/startOB.txt";
         $lines = file($myFile);
-        $linesOB = file($startOB);
         $tomorrow = $lines[0];
         ?>
         <meta charset="utf-8">
@@ -60,21 +56,55 @@ body, html {
   padding: 20px;
   text-align: center;
 }
+
+.myButton {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #44c767), color-stop(1, #5cbf2a));
+	background:-moz-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+	background:-webkit-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+	background:-o-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+	background:-ms-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+	background:linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#44c767', endColorstr='#5cbf2a',GradientType=0);
+	background-color:#44c767;
+	-moz-border-radius:9px;
+	-webkit-border-radius:9px;
+	border-radius:9px;
+	border:1px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:12px;
+	font-weight:bold;
+	padding:7px 7px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cbf2a), color-stop(1, #44c767));
+	background:-moz-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+	background:-webkit-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+	background:-o-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+	background:-ms-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+	background:linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cbf2a', endColorstr='#44c767',GradientType=0);
+	background-color:#5cbf2a;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
 </style>
 
     </head>
-    <a href="/admin/">Admin</a>
+    <a href="/admin/" class="myButton">Admin</a>
     <body>
         <div class="bg-image"></div>
         <div class="bg-text">
         <h2 style="text-align:center;">Preload start time for <?php echo($tomorrow);?></h2>
         <h3 style="text-align:center">Prime: <?php echo($lines[1]);?>AM</h3>
         <h3 style="text-align:center">Start: <?php echo($lines[2]);?> AM</h3>
-        <h3 style="text-align:center"><?php echo($lines[3]);?>
-        <h2 style="text-align:center;">Preload start time for <?php echo($tomorrow);?></h2>
-        <h3 style="text-align:center">Prime: <?php echo($lines[1]);?>AM</h3>
-        <h3 style="text-align:center">Start: <?php echo($lines[2]);?> AM</h3>
-        <h3 style="text-align:center"><?php echo($lines[3]);?>            
+        <h3 style="text-align:center"><?php echo($lines[3]);?>          
         </div>
     </body>
     
