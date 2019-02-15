261f3dd62c7709fae903aa5dab51e122d22d83c4
Unknown <kjc008@aquinas.edu>
2019-02-15 10:37:22 -0500

    Prime field hides when empty, small syntax errors fixed

--- index.php
+++ index.php
@@ -1,113 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Start Time</title>
-        <!-- TODO: Selectable day?? -->
-        <?php ; 
+
+<head>
+    <title>Start Time</title>
+    <!-- TODO: Selectable day?? -->
+    <?php ; 
         $myFile = "admin/start.txt";
         $lines = file($myFile);
         $tomorrow = $lines[0];
         ?>
-        <meta charset="utf-8">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body, html {
-  height: 100%;
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
 
-* {
-  box-sizing: border-box;
-}
+        * {
+            box-sizing: border-box;
+        }
 
-.bg-image {
-  /* The image used */
-  background-image: url("background.jpg");
-  
-  /* Add the blur effect */
-  filter: blur(8px);
-  -webkit-filter: blur(8px);
-  
-  /* Full height */
-  height: 100%; 
-  
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
+        .bg-image {
+            /* The image used */
+            background-image: url("background.jpg");
 
-/* Position text in the middle of the page/image */
-.bg-text {
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
-  font-family: 'Roboto', sans-serif;
-  color: white;
-  font-weight: bold;
-  border: 3px solid #f1f1f1;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 2;
-  width: 80%;
-  padding: 20px;
-  text-align: center;
-}
+            /* Add the blur effect */
+            filter: blur(8px);
+            -webkit-filter: blur(8px);
 
-.myButton {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #44c767), color-stop(1, #5cbf2a));
-	background:-moz-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-	background:-webkit-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-	background:-o-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-	background:-ms-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-	background:linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#44c767', endColorstr='#5cbf2a',GradientType=0);
-	background-color:#44c767;
-	-moz-border-radius:9px;
-	-webkit-border-radius:9px;
-	border-radius:9px;
-	border:1px solid #18ab29;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:12px;
-	font-weight:bold;
-	padding:7px 7px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #2f6627;
-}
-.myButton:hover {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cbf2a), color-stop(1, #44c767));
-	background:-moz-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-	background:-webkit-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-	background:-o-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-	background:-ms-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-	background:linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cbf2a', endColorstr='#44c767',GradientType=0);
-	background-color:#5cbf2a;
-}
-.myButton:active {
-	position:relative;
-	top:1px;
-}
-</style>
+            /* Full height */
+            height: 100%;
 
-    </head>
-    <a href="/admin/" class="myButton">Admin</a>
-    <body>
-        <div class="bg-image"></div>
-        <div class="bg-text">
-        <h2 style="text-align:center;">Preload start time for <?php echo($tomorrow);?></h2>
-        <h3 style="text-align:center">Prime: <?php echo($lines[1]);?>AM</h3>
-        <h3 style="text-align:center">Start: <?php echo($lines[2]);?> AM</h3>
-        <h3 style="text-align:center"><?php echo($lines[3]);?>          
-        </div>
-    </body>
-    
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        /* Position text in the middle of the page/image */
+        .bg-text {
+            background-color: rgb(0, 0, 0);
+            /* Fallback color */
+            background-color: rgba(0, 0, 0, 0.4);
+            /* Black w/opacity/see-through */
+            font-family: 'Roboto', sans-serif;
+            color: white;
+            font-weight: bold;
+            border: 3px solid #f1f1f1;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 2;
+            width: 80%;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .myButton {
+            background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #44c767), color-stop(1, #5cbf2a));
+            background: -moz-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+            background: -webkit-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+            background: -o-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+            background: -ms-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
+            background: linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
+            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#44c767', endColorstr='#5cbf2a', GradientType=0);
+            background-color: #44c767;
+            -moz-border-radius: 9px;
+            -webkit-border-radius: 9px;
+            border-radius: 9px;
+            border: 1px solid #18ab29;
+            display: inline-block;
+            cursor: pointer;
+            color: #ffffff;
+            font-family: Arial;
+            font-size: 12px;
+            font-weight: bold;
+            padding: 7px 7px;
+            text-decoration: none;
+            text-shadow: 0px 1px 0px #2f6627;
+        }
+
+        .myButton:hover {
+            background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cbf2a), color-stop(1, #44c767));
+            background: -moz-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+            background: -webkit-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+            background: -o-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+            background: -ms-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
+            background: linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
+            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cbf2a', endColorstr='#44c767', GradientType=0);
+            background-color: #5cbf2a;
+        }
+
+        .myButton:active {
+            position: relative;
+            top: 1px;
+        }
+
+    </style>
+
+</head>
+<a href="/admin/" class="myButton">Admin</a>
+
+<body>
+    <div class="bg-image"></div>
+    <div class="bg-text">
+        <h2 style="text-align:center;">Preload start time for
+            <?php echo($tomorrow);?>
+        </h2>
+        <?php if ($lines[1] != "\n"): ?>
+        <h3 style="text-align:center">Prime:
+            <?php echo($lines[1]);?>AM</h3>
+        <?php endif ?>
+        <h3 style="text-align:center">Start:
+            <?php echo($lines[2]);?> AM</h3>
+        <h3 style="text-align:center">
+            <?php echo($lines[3]);?>
+        </h3>
+    </div>
+</body>
+
 </html>
-
-
