0c72a8b540a8a5ff53c6c7946523eb8fdc9914ca
Kevin <clintkev251@gmail.com>
2019-06-14 13:50:42 -0400

    I suck at git... still release 2.0

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script>
+        import {
+            MDCRipple
+        } from '@material/ripple';
+
+        const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+    </script>
     <title>Start Time</title>
     <!-- TODO: Selectable day?? -->
     <?php ; 
@@ -23,138 +34,80 @@
     <link rel="apple-touch-icon" sizes="144x144" href="/img/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="/img/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/img/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     <link rel="manifest" href="/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="/img/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <style>
-        body,
-        html {
-            height: 100%;
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
+        h2,
+        h3 {
+            font-family: "Roboto", "serif"
         }
 
-        * {
-            box-sizing: border-box;
+        a {
+            text-decoration: none;
+            color: inherit;
+        }
+
+        body {
+            background-image: url("/img/background.png");
+            background-repeat: no-repeat;
+            background-attachment: fixed;
         }
 
         .bg-image {
             /* The image used */
-            background-image: url("background.jpg");
+            background-image: url("/img/background.png");
 
             /* Add the blur effect */
             filter: blur(8px);
             -webkit-filter: blur(3px);
-
-            /* Full height */
-            height: 100%;
-
-
-
-            /* Center and scale the image nicely */
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
+            background-attachment: fixed
         }
 
-        /* Position text in the middle of the page/image */
-        .bg-text {
-            background-color: rgb(0, 0, 0);
-            /* Fallback color */
-            background-color: rgba(0, 0, 0, 0.4);
-            /* Black w/opacity/see-through */
-            font-family: 'Roboto', sans-serif;
-            color: white;
-            font-size: 14px;
-            font-weight: normal;
-            border: 3px solid #f1f1f1;
-            border-radius: 10px;
-            position: absolute;
-            top: 30%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 2;
-            width: 80%;
+        .mdc-card {
+            margin: 2%;
             padding: 1%;
-            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center
+        }
         }
 
-        .bg-textRel {
-            background-color: rgb(0, 0, 0);
-            /* Fallback color */
-            background-color: rgba(0, 0, 0, 0.4);
-            /* Black w/opacity/see-through */
-            font-family: 'Roboto', sans-serif;
-            color: white;
-            font-size: 14px;
-            font-weight: normal;
-            border: 3px solid #f1f1f1;
-            border-radius: 10px;
-            position: absolute;
-            top: 80%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 0;
-            width: 80%;
-            padding: 1%;
-            text-align: center;
+        .mdc-button__label {
+            font-family: "Roboto", "sarif";
         }
 
-        .myButton {
-            background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #44c767), color-stop(1, #5cbf2a));
-            background: -moz-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-            background: -webkit-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-            background: -o-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-            background: -ms-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-            background: linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
-            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#44c767', endColorstr='#5cbf2a', GradientType=0);
-            background-color: #44c767;
-            -moz-border-radius: 9px;
-            -webkit-border-radius: 9px;
-            border-radius: 9px;
-            border: 1px solid #18ab29;
-            display: inline-block;
-            cursor: pointer;
-            color: #ffffff;
-            font-family: Arial;
-            font-size: 12px;
-            font-weight: bold;
-            padding: 7px 7px;
-            text-decoration: none;
-            text-shadow: 0px 1px 0px #2f6627;
-            position: absolute;
-            z-index: 1;
-        }
 
-        .myButton:hover {
-            background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cbf2a), color-stop(1, #44c767));
-            background: -moz-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-            background: -webkit-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-            background: -o-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-            background: -ms-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-            background: linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
-            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cbf2a', endColorstr='#44c767', GradientType=0);
-            background-color: #5cbf2a;
-        }
+        @media (max-width: 490px) {
+            .mdc-card {
+                top: 50%;
+                /* IMPORTANT */
+                left: 50%;
+                /* IMPORTANT */
+                width: 100%;
+                margin-left: -1px;
+                /* HALF OF THE WIDTH */
+                margin-right: 100%;
+            }
 
-        .myButton:active {
-            position: absolute;
-            z-index: 1;
-        }
+            .bg-image {
+                background-image: url("/img/background-mobile.png");
+            }
 
+        }
     </style>
 
 </head>
-<a href="/admin/" class="myButton">Admin</a>
+<button class="mdc-button mdc-button--raised"><a href="/admin">Admin</a></button>
 
 <body>
-    <div class="bg-image"></div>
-    <div class="bg-text">
+
+    <div class="mdc-card">
         <h2 style="text-align:center;">Preload start time for
             <?php echo($tomorrow);?>
         </h2>
@@ -167,8 +120,8 @@
         <h3 style="text-align:center">
             <?php echo($lines[3]);?>
         </h3>
-        </div>
-        <div class="bg-textRel">
+    </div>
+    <div class="mdc-card">
         <h2 style="text-align:center;">Outbound start time for <?php echo($linesOB[0]);?></h2>
         <?php if($linesOB[1] != "\n"): ?>
         <h3 style="text-align:center">Prime: <?php echo($linesOB[1]);?>PM</h3>
@@ -180,7 +133,7 @@
         <h3 style="text-align:center">Smalls: <?php echo($linesOB[3]);?>PM</h3>
         <?php endif ?>
         <h3 style="text-align:center">Start: <?php echo($linesOB[4]);?> PM</h3>
-        <h3 style="text-align:center"><?php echo($linesOB[5]);?>     
+        <h3 style="text-align:center"><?php echo($linesOB[5]);?>
     </div>
 </body>
 
