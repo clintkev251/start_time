0c2577048d9df821db4be86521006c1463b921fa
Kevin <clintkev251@gmail.com>
2020-02-20 16:50:07 +0000

    Timezone corrected

--- config.php
+++ config.php
@@ -13,5 +13,5 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
+mysqli_query($link, "SET time_zone = '-5:00';");
 ?>
