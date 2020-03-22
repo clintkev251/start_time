c6e3d504064963c9f2eacfa5f5b08b2ee2ebc36c
Kevin <clintkev251@gmail.com>
2020-03-22 09:03:59 +0000

    Changes to adapt for ral

--- config.php
+++ config.php
@@ -1,10 +1,8 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'sql.start-time.com');
 define('DB_USERNAME', 'start_login');
 define('DB_PASSWORD', '_mMk_g!sHUF_XUb4uMcW');
-define('DB_NAME', 'start_nodelogin');
+define('DB_NAME', 'ralstart_db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
