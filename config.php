923874ee86c4cd5e6d91918e03b9559307d6f665
Kevin <clintkev251@gmail.com>
2020-05-20 08:11:53 +0000

    Config updated

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'sql.start-time.com');
 define('DB_USERNAME', 'start_login');
 define('DB_PASSWORD', '_mMk_g!sHUF_XUb4uMcW');
@@ -12,4 +14,39 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 mysqli_query($link, "SET time_zone = 'US/Eastern';");
+
+
+// Sort Options
+
+// Basic config
+$stationName = "ZRAL";
+$preloadFlag = true;
+$outboundFlag = true;
+$otpFlag = false;
+
+// Preload fields
+
+$preloadUnloadFlag = false;
+$preloadVanlinesFlag = false;
+$preloadPrimeFlag = true;
+$preloadSmallsFlag = false;
+$preloadStartFlag = true;
+
+// Outbound fields
+
+$outboundUnloadFlag = false;
+$outboundVanlinesFlag = false;
+$outboundPrimeFlag = true;
+$outboundSmallsFlag = false;
+$outboundStartFlag = true;
+
+// OTP options
+
+$otpUnloadFlag = false;
+$otpVanlinesFlag = false;
+$otpPrimeFlag = false;
+$otpSmallsFlag = false;
+$otpStartFlag = false;
+
+
 ?>
