5412791c48c67c48055a1e7ca14715b19b457eca
Kevin <clintkev251@gmail.com>
2020-07-08 10:36:49 +0000

    Major feature update: Unified start system, moved to new database, features added for switching bettween locations

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'sql.start-time.com');
 define('DB_USERNAME', 'start_login');
 define('DB_PASSWORD', '_mMk_g!sHUF_XUb4uMcW');
-define('DB_NAME', 'start_nodelogin');
+define('DB_NAME', 'starttimedistributed');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -13,39 +13,6 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 mysqli_query($link, "SET time_zone = 'US/Eastern';");
-
-
-// Sort Options
-
-// Basic config
-$stationName = "ZGRD";
-$preloadFlag = true;
-$outboundFlag = true;
-$otpFlag = false;
-
-// Preload fields
-
-$preloadUnloadFlag = true;
-$preloadVanlinesFlag = false;
-$preloadPrimeFlag = true;
-$preloadSmallsFlag = true;
-$preloadStartFlag = true;
-
-// Outbound fields
-
-$outboundUnloadFlag = false;
-$outboundVanlinesFlag = true;
-$outboundPrimeFlag = true;
-$outboundSmallsFlag = true;
-$outboundStartFlag = true;
-
-// OTP options
-
-$otpUnloadFlag = false;
-$otpVanlinesFlag = false;
-$otpPrimeFlag = false;
-$otpSmallsFlag = false;
-$otpStartFlag = false;
 
 
 ?>
--- index.php
+++ index.php
@@ -3,18 +3,27 @@
 
 <?php
 require_once "config.php";
+
+$stationNumber = $_COOKIE["stationViewer"];
+
 // Get existing data from database - Preload
-$sql = mysqli_query($link, "SELECT * FROM times WHERE sort = 'preload'");
+$sql = mysqli_query($link, "SELECT * FROM times WHERE sort = 'Preload' AND stationNumber = $stationNumber");
 $preloadTimes = mysqli_fetch_assoc($sql);
 
+$sql = mysqli_query($link, "SELECT * FROM fieldLegend WHERE sort = 'Preload' AND stationNumber = $stationNumber");
+$preloadFields = mysqli_fetch_assoc($sql);
+
 // Outbound
-$sql = mysqli_query($link, "SELECT * FROM times WHERE sort = 'outbound'");
+$sql = mysqli_query($link, "SELECT * FROM times WHERE sort = 'Outbound' AND stationNumber = $stationNumber");
 $outboundTimes = mysqli_fetch_assoc($sql);
 
 // OTP
-// Outbound
-$sql = mysqli_query($link, "SELECT * FROM times WHERE sort = 'otp'");
+$sql = mysqli_query($link, "SELECT * FROM times WHERE sort = 'OTP' AND stationNumber = $stationNumber");
 $otpTimes = mysqli_fetch_assoc($sql);
+
+
+$sql = mysqli_query($link, "SELECT * FROM stations WHERE stationNumber = $stationNumber");
+$stationConfig = mysqli_fetch_assoc($sql);
 
 include "head.php";
 
@@ -22,91 +31,119 @@ include "head.php";
 
 
 <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored" href="admin.php">Admin</a>
+<a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored" href="select-station.php">Change Location</a>
 
 <body>
-    <?php if($preloadFlag){ ?>
+    <?php if($stationConfig['preloadFlag'] == 'y'){ ?>
         <div class="mdc-card">
             <h4>Preload start time for
                 <?php echo($preloadTimes["date"]);?>
             </h4>
-            <?php if ($preloadTimes["unload"] != ""): ?>
-            <h6>Unload:
-                <?php echo($preloadTimes["unload"]);?> </h6>
+            <?php if ($preloadTimes["field1"] != ""): ?>
+            <h6><?php echo $preloadFields["field1"] , ": ", $preloadTimes["field1"];?> </h6>
             <?php endif ?>
-            <?php if($preloadTimes["vanlines"] != ""): ?>
-            <h6>Vanlines: <?php echo($preloadTimes["vanlines"]);?> </h6>
+            <?php if($preloadTimes["field2"] != ""): ?>
+            <h6><?php echo $preloadFields['field2'] , ": " , $preloadTimes["field2"];?> </h6>
             <?php endif ?>
-            <?php if ($preloadTimes["prime"] != ""): ?>
-            <h6>Prime:
-                <?php echo($preloadTimes["prime"]);?> </h6>
+            <?php if ($preloadTimes["field3"] != ""): ?>
+            <h6><?php echo $preloadFields['field3'] , ": "  , $preloadTimes["field3"];?> </h6>
             <?php endif ?>
-            <?php if ($preloadTimes["smalls"] != ""): ?>
-            <h6>Smalls:
-                <?php echo($preloadTimes["smalls"]);?> </h6>
+            <?php if ($preloadTimes["field4"] != ""): ?>
+            <h6> <?php echo $preloadFields["field4"] , ": " , $preloadTimes["field4"];?> </h6>
             <?php endif ?>
-            <h6>Start:
-                <?php echo($preloadTimes["start"]);?> </h6>
+            <?php if ($preloadTimes["field5"] != ""): ?>
+            <h6> <?php echo $preloadFields["field5"] , ": " , $preloadTimes["field5"];?> </h6>
+            <?php endif ?>
+            <?php if ($preloadTimes["field6"] != ""): ?>
+            <h6> <?php echo $preloadFields["field6"] , ": " , $preloadTimes["field6"];?> </h6>
+            <?php endif ?>
+            <?php if ($preloadTimes["field7"] != ""): ?>
+            <h6> <?php echo $preloadFields["field7"] , ": " , $preloadTimes["field7"];?> </h6>
+            <?php endif ?>
+            <?php if ($preloadTimes["field8"] != ""): ?>
+            <h6> <?php echo $preloadFields["field8"] , ": " , $preloadTimes["field8"];?> </h6>
+            <?php endif ?>
+            <?php if ($preloadTimes["field9"] != ""): ?>
+            <h6> <?php echo $preloadFields["field9"] , ": " , $preloadTimes["field9"];?> </h6>
+            <?php endif ?>
             <?php if($preloadTimes["notes"] != ""): ?>
-            <h6>
-                <?php echo($preloadTimes["notes"]); ?>
-            </h6>
+            <h6><?php echo($preloadTimes["notes"]); ?></h6>
             <?php endif ?>
         </div>
     <?php }
-    if($outboundFlag){ ?>
+    if($stationConfig['outboundFlag']=='y'){ ?>
         <div class="mdc-card">
-            <h4>Outbound start time for <?php echo($outboundTimes["date"]);?></h4>
-            <?php if ($outboundTimes["unload"] != ""): ?>
-            <h6>Unload:
-                <?php echo($outboundTimes["unload"]);?> </h6>
+            <h4>Outbound start time for     
+            <?php echo($outboundTimes["date"]);?>
+            </h4>
+            <?php if ($outboundTimes["field1"] != ""): ?>
+            <h6><?php echo $outboundFields["field1"] , ": ", $outboundTimes["field1"];?> </h6>
             <?php endif ?>
-            <?php if($outboundTimes["vanlines"] != ""): ?>
-            <h6>Vanlines: <?php echo($outboundTimes["vanlines"]);?> </h6>
+            <?php if($outboundTimes["field2"] != ""): ?>
+            <h6><?php echo $outboundFields['field2'] , ": " , $outboundTimes["field2"];?> </h6>
             <?php endif ?>
-            <?php if ($outboundTimes["prime"] != ""): ?>
-            <h6>Prime:
-                <?php echo($outboundTimes["prime"]);?> </h6>
+            <?php if ($outboundTimes["field3"] != ""): ?>
+            <h6><?php echo $outboundFields['field3'] , ": "  , $outboundTimes["field3"];?> </h6>
             <?php endif ?>
-            <?php if ($outboundTimes["smalls"] != ""): ?>
-            <h6>Smalls:
-                <?php echo($outboundTimes["smalls"]);?> </h6>
+            <?php if ($outboundTimes["field4"] != ""): ?>
+            <h6> <?php echo $outboundFields["field4"] , ": " , $outboundTimes["field4"];?> </h6>
             <?php endif ?>
-            <h6>Start:
-                <?php echo($outboundTimes["start"]);?> </h6>
+            <?php if ($outboundTimes["field5"] != ""): ?>
+            <h6> <?php echo $outboundFields["field5"] , ": " , $outboundTimes["field5"];?> </h6>
+            <?php endif ?>
+            <?php if ($outboundTimes["field6"] != ""): ?>
+            <h6> <?php echo $outboundFields["field6"] , ": " , $outboundTimes["field6"];?> </h6>
+            <?php endif ?>
+            <?php if ($outboundTimes["field7"] != ""): ?>
+            <h6> <?php echo $outboundFields["field7"] , ": " , $outboundTimes["field7"];?> </h6>
+            <?php endif ?>
+            <?php if ($outboundTimes["field8"] != ""): ?>
+            <h6> <?php echo $outboundFields["field8"] , ": " , $outboundTimes["field8"];?> </h6>
+            <?php endif ?>
+            <?php if ($outboundTimes["field9"] != ""): ?>
+            <h6> <?php echo $outboundFields["field9"] , ": " , $outboundTimes["field9"];?> </h6>
+            <?php endif ?>
             <?php if($outboundTimes["notes"] != ""): ?>
-            <h6>
-                <?php echo($outboundTimes["notes"]); ?>
-            </h6>
+            <h6><?php echo($outboundTimes["notes"]); ?></h6>
             <?php endif ?>
-        </div>
+            </div>
     <?php }
-    if($otpFlag){ ?>
+    if($stationConfig['otpFlag'] == 'y'){ ?>
         <div class="mdc-card">
-            <h4>OTP start time for <?php echo($otpTimes["date"]);?></h4>
-            <?php if ($otpTimes["unload"] != ""): ?>
-            <h6>Unload:
-                <?php echo($otpTimes["unload"]);?> </h6>
+            <h4>OTP start time for
+            <?php echo($otpTimes["date"]);?>
+            </h4>
+            <?php if ($otpTimes["field1"] != ""): ?>
+            <h6><?php echo $otpFields["field1"] , ": ", $otpTimes["field1"];?> </h6>
             <?php endif ?>
-            <?php if($otpTimes["vanlines"] != ""): ?>
-            <h6>Vanlines: <?php echo($otpTimes["vanlines"]);?> </h6>
+            <?php if($otpTimes["field2"] != ""): ?>
+            <h6><?php echo $otpFields['field2'] , ": " , $otpTimes["field2"];?> </h6>
             <?php endif ?>
-            <?php if ($otpTimes["prime"] != ""): ?>
-            <h6>Prime:
-                <?php echo($otpTimes["prime"]);?> </h6>
+            <?php if ($otpTimes["field3"] != ""): ?>
+            <h6><?php echo $otpFields['field3'] , ": "  , $otpTimes["field3"];?> </h6>
             <?php endif ?>
-            <?php if ($otpTimes["smalls"] != ""): ?>
-            <h6>Smalls:
-                <?php echo($otpTimes["smalls"]);?> </h6>
+            <?php if ($otpTimes["field4"] != ""): ?>
+            <h6> <?php echo $otpFields["field4"] , ": " , $otpTimes["field4"];?> </h6>
             <?php endif ?>
-            <h6>Start:
-                <?php echo($otpTimes["start"]);?> </h6>
+            <?php if ($otpTimes["field5"] != ""): ?>
+            <h6> <?php echo $otpFields["field5"] , ": " , $otpTimes["field5"];?> </h6>
+            <?php endif ?>
+            <?php if ($otpTimes["field6"] != ""): ?>
+            <h6> <?php echo $otpFields["field6"] , ": " , $otpTimes["field6"];?> </h6>
+            <?php endif ?>
+            <?php if ($otpTimes["field7"] != ""): ?>
+            <h6> <?php echo $otpFields["field7"] , ": " , $otpTimes["field7"];?> </h6>
+            <?php endif ?>
+            <?php if ($otpTimes["field8"] != ""): ?>
+            <h6> <?php echo $otpFields["field8"] , ": " , $otpTimes["field8"];?> </h6>
+            <?php endif ?>
+            <?php if ($otpTimes["field9"] != ""): ?>
+            <h6> <?php echo $otpFields["field9"] , ": " , $otpTimes["field9"];?> </h6>
+            <?php endif ?>
             <?php if($otpTimes["notes"] != ""): ?>
-            <h6>
-                <?php echo($otpTimes["notes"]); ?>
-            </h6>
+            <h6><?php echo($otpTimes["notes"]); ?></h6>
             <?php endif ?>
-        </div>
+            </div>
     <?php } ?>
 </body>
-
 </html>
